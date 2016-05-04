7b2d9d47cdef2c266dca79c9c698bf16c87c7739
alvarocmh <alvarocmh@outlook.es>
2016-05-04 21:39:23 +0200

    Subida diseño pagina principal utilizando framework Semantic UI

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	//Funcion para Modal de Iniciar Sesión
+	$(".login.button").click(function() {
+		$('.small.modal').modal({
+			blurring: true
+		}).modal('show');
+	});
+	
+});
